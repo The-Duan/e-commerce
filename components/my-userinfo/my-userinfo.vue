bb0d518f2306fc956fafd2a92397fede6d8385d7
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-07-14 19:41:58 +0800

    登录（登录页面）

---
+++ components/my-userinfo/my-userinfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+信息组件    
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-userinfo",
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
